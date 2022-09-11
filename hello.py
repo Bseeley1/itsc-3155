bf0c9e7cc813385829272e7a460e682cd9d6b8e6
Bseeley1 <seeleybrian2000@gmail.com>
2022-09-10 21:18:25 -0400

    "First Commit"

--- hello.py
+++ hello.py
@@ -1,1 +1,9 @@
-print('Hello World')
+userInput = input("Original price: ")
+price = float(userInput)
+
+if (price >= 10) :
+   discountedPrice = price - 3.14
+else :
+   discountedPrice = price - 1         
+
+print("Discounted price: %.2f\n" % discountedPrice)
